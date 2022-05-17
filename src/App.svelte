f6969291c31f71fa89543011bc7aa428e840b3e6
Andy Li <1450947+andy1li@users.noreply.github.com>
2022-05-18 07:49:31 +0800

    For bug report

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { createMachine, assign } from '@xstate/fsm'
+  import { useMachine } from '@xstate/svelte/lib/fsm'
+
+  const machine = createMachine({
+    id: 'machine',
+    initial: 'initial',
+    states: {
+      initial: {},
+    },
+  })
+
+  const { state, send } = useMachine(machine)
+  
+  console.log($state.value)
+  console.log(send)
+</script>
+
+<div>
+  Please check the error message in the console.
+</div>
